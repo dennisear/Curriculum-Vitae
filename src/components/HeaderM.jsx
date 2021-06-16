7104dc338f7df51e3717e953597f70c52ae334b4
Dennis_Arango <dennis.arango.mt@correo.usa.edu.co>
2021-06-16 08:08:37 -0500

    Responsive header design

--- src/components/HeaderM.jsx
+++ src/components/HeaderM.jsx
@@ -1,6 +1,6 @@
-import React from 'react' // importar SIEMPRE react //
-import {Link} from 'react-router-dom'
-import '../utils/css/headerMStyles.css'
+import React from 'react' // import ALWAYS react //
+import {Link} from 'react-router-dom' //Declaration//
+import '../utils/css/headerMStyles.css' //Styles//
 import Logo from '../utils/images/logo.png'
 import Banner from '../utils/images/banner.jpg'
 
@@ -12,15 +12,14 @@ const HeaderM = () => {
             </div>
             <div className="Menu"> 
                 <Link to='/Home'>
-                    <button type="button" id="boton" className="grow_spin">Formación académica</button>
+                    <button type="button" id="boton" className="grow_spin">Formación académica</button> {/* Button that takes me to the page 'formación académica' */}
                 </Link>
                 <Link to='AboutMe/'>
-                    <button type="button" id="boton" className="grow_spin">Sobre mí</button>
+                    <button type="button" id="boton" className="grow_spin">Sobre mí</button> {/* Button that takes me to the page about me */}
                 </Link>
-                <Link to="BodyM/">
-                    <button type="button" id="boton" className="grow_spin">Conoce mi trabajo</button>
+                <Link to='BodyM/'>
+                    <button type="button" id="boton" className="grow_spin">Conoce mi trabajo</button> {/* Button that takes me to the component body*/}
                </Link>
-
             </div>
             <div className="Banner">
                <img src={Banner} alt="Banner"></img>
